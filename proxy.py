e73b44460e77d14c5f8834efcbed81a1ac30428c
iBAloFA <seyioflmu@gmail.com>
2026-09-12 13:29:25 +0100

    fix: import time

--- proxy.py
+++ proxy.py
@@ -2,6 +2,7 @@ import os
 import json
 import base64
 import httpx
+import time
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from nacl.signing import VerifyKey
